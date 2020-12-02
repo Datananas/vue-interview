ef2c4401ca5f12f29dda6ce115069a167386758b
Romain SIMON <contact@romainsimon.net>
2020-12-02 13:33:37 +0100

    New: initial commit

---
+++ src/components/SearchField.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Build a search input component here -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'SearchField'
+}
+</script>
+
+<style lang="scss">
+</style>
